a245d4c85a81950fa8da4c644f22c552467e72c8
Ahmed <ahme.abdallah124@gmail.com>
2023-07-05 03:10:03 +0300

    background changed

--- app.py
+++ app.py
@@ -7,23 +7,20 @@ cssfile = current_dir / "styles" / "style.css"
 resume_file = current_dir / "assets" / "Ahmed Abdallah.pdf"
 profile_pic = current_dir / "assets" / "av2.jpg"
 
-print(cssfile)
-
 page_title = "Ahmed's Resume"
-page_icon = "✋"
 name = "Ahmed Abdallah"
 Description = "Fresh Data Analyst"
 email = "ahmed.abdallah10024@gmail.com"
 social_media = {
     "Linkedin": "https://www.linkedin.com/in/ahmed4abdallah4fever/",
     "GitHub": "https://github.com/ahmed-2001M",
-    }
+}
 
-st.set_page_config(page_title=page_title, page_icon="✋")
-
-
+st.set_page_config(page_title=page_title)
 
 
+with open(cssfile) as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 with open(resume_file, "rb") as pdf_file:
     pdfbyte = pdf_file.read()
@@ -31,18 +28,15 @@ with open(resume_file, "rb") as pdf_file:
 profile_pic = Image.open(profile_pic)
 
 
-col1, col2 = st.columns([2,4], gap="small")
+col1, col2 = st.columns([2, 4], gap="small")
 with col1:
     st.image(
         profile_pic,
         use_column_width=True,
         output_format="JPEG",
         channels="RGB",
-        caption="Profile Picture",
         clamp=False,
-        width=150,
-        )
-    # st.markdown("<style>img{width:150px}</style>", unsafe_allow_html = True)
+    )
 
 with col2:
     st.title(name)
@@ -54,16 +48,9 @@ with col2:
         mime="application/octet-stream",
     )
     st.write("📫", email)
-    
-    
-    
+
 st.write("#")
+
 cols = st.columns(len(social_media))
-for idx,(platform, link) in enumerate(social_media.items()):
+for idx, (platform, link) in enumerate(social_media.items()):
     cols[idx].write(f"[{platform}]({link})")
-
-
-
-
-with open(cssfile) as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
